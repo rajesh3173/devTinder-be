9d064d38e8493f87028a3d7980004aae94c47a05
Ruttala Rajesh Kumar <ruttala.rajesh111@gmail.com>
2026-07-01 13:55:29 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const { connectDb } = require("./config/database");
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -8,6 +9,12 @@ const userRouter = require("./routes/user");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -45,7 +45,7 @@ authRouter.post("/login", async (req, res) => {
       const token = await user.getJWT();
 
       res.cookie("token", token, { maxAge: 1000 * 60 * 60 * 24 });
-      res.send("login success");
+      res.send({ data: user });
     }
   } catch (error) {
     res.status(400).send("Error: " + error.message);
